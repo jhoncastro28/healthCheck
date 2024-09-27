993f571bbd5ba2f4498f16fb5dc34b464bb750fa
Jhon Castro <jhon.castro07@uptc.edu.co>
2024-09-26 19:25:43 -0500

    Se agrega la funcionalidad de SetInterval y se configurar dependencias del proyecto

--- scripts/main.js
+++ scripts/main.js
@@ -1,20 +1,42 @@
-document.getElementById('checkStatusBtn').addEventListener('click', () => {
-    const responseDiv = document.getElementById('response');
-    responseDiv.textContent = 'Checking...';
+let intervalId;
 
-    fetch('/middleware-endpoint')
-        .then(response => response.json())
-        .then(data => {
-            responseDiv.innerHTML = '';
-            data.forEach(serverData => {
-                const serverInfo = `
-                    <p>${serverData.server}: ${serverData.status} - ${serverData.timestamp}</p>
-                `;
-                responseDiv.innerHTML += serverInfo;
+document.getElementById('startCheckBtn').addEventListener('click', () => {
+    const intervalInput = document.getElementById('intervalInput').value;
+    const responseDiv = document.getElementById('response');
+
+    const interval = parseInt(intervalInput);
+
+    if (isNaN(interval) || interval <= 0) {
+        responseDiv.textContent = 'Por favor, ingrese un intervalo válido en milisegundos.';
+        return;
+    }
+
+    if (intervalId) {
+        clearInterval(intervalId);
+    }
+    const checkStatus = () => {
+        responseDiv.textContent = 'Checking...';
+
+        fetch('/middleware-endpoint')
+            .then(response => response.json())
+            .then(data => {
+                responseDiv.innerHTML = '';
+                data.forEach(serverData => {
+                    const serverInfo = `
+                        <p>${serverData.server}: ${serverData.status} - ${serverData.timestamp}</p>
+                    `;
+                    responseDiv.innerHTML += serverInfo;
+                });
+            })
+            .catch(error => {
+                responseDiv.textContent = 'Error communicating with the middleware';
+                console.error('Error:', error);
             });
-        })
-        .catch(error => {
-            responseDiv.textContent = 'Error communicating with the middleware';
-            console.error('Error:', error);
-        });
+    };
+
+    // Iniciar el proceso de verificación tantos milisegundos
+    intervalId = setInterval(checkStatus, interval);
+
+    // Ejecutar inmediatamente la primera verificación
+    checkStatus();
 });
