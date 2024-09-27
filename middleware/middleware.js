6428d87ae686524e048add93465e139e950ce516
Jhon Castro <jhon.castro07@uptc.edu.co>
2024-09-26 19:44:26 -0500

    Se solucionan conflictos con rutas de los archivos

--- middleware/middleware.js
+++ middleware/middleware.js
@@ -1,9 +1,10 @@
 const express = require('express');
+const path = require('path');
 const fetch = require('node-fetch');
 const app = express();
 const port = 3000;
 
-app.use(express.static('public'));
+app.use(express.static(path.join(__dirname, '..')));
 
 app.get('/middleware-endpoint', async (req, res) => {
     try {
